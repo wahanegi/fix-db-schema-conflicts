f0b05bedc0ae282e9f9a4573bfb7f29e35c3ea4a
Roger Graves <rogergraves@gmail.com>
2023-05-01 15:54:49 +0100

    CP-977: Modify forked gem to ignore secondary database tables on main dump based on pattern in name

--- lib/fix_db_schema_conflicts/schema_dumper.rb
+++ lib/fix_db_schema_conflicts/schema_dumper.rb
@@ -16,7 +16,7 @@ module FixDBSchemaConflicts
       end
 
       def foreign_keys(table)
-        __getobj__.indexes(table).sort_by(&:name)
+        __getobj__.foreign_keys(table).sort_by(&:name)
       end
     end
 
@@ -37,6 +37,43 @@ module FixDBSchemaConflicts
       result = yield
       @connection = old
       result
+    end
+
+    def filtered_tables
+      @connection.tables.reject { |table| table.start_with?(ENV['PRECEDES_SECONDARY_DB_TABLE_NAMES']) || %w[schema_migrations ar_internal_metadata].include?(table) }.sort
+    end
+
+    def dump(stream)
+      with_sorting do
+        header(stream)
+        extensions(stream)
+        tables(stream)
+        trailer(stream)
+        stream
+      end
+    end
+
+    def tables(stream)
+      filtered_tables.each do |tbl|
+        table(tbl, stream)
+      end
+      foreign_keys(filtered_tables, stream)
+    end
+
+    def foreign_keys(tables, stream)
+      all_foreign_keys = tables.flat_map { |table_name| @connection.foreign_keys(table_name) }
+      return if all_foreign_keys.empty?
+
+      stream.puts
+      all_foreign_keys.sort_by { |fk| [fk.from_table, fk.to_table, fk.name] }.each do |foreign_key|
+        statement = "  add_foreign_key #{remove_prefix_and_suffix(foreign_key.from_table).inspect}, #{remove_prefix_and_suffix(foreign_key.to_table).inspect}"
+        default_column_name = "#{remove_prefix_and_suffix(foreign_key.to_table).singularize}_id"
+
+        statement << ", column: #{foreign_key.options[:column].inspect}" if foreign_key.options[:column] && foreign_key.options[:column] != default_column_name
+
+        stream.puts statement
+      end
+      stream.puts
     end
   end
 end
